a4b56714e035ccb06e7bbb3f913cb7a94394b755
61070055 <61070055@kmitl.ac.th>
2020-05-07 21:25:02 +0700

    Report check

--- app/report.tsx
+++ app/report.tsx
@@ -2,96 +2,101 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Switch, Dimensions, TouchableHighlight, Button, Alert } from 'react-native';
 import { TextInput, ScrollView } from 'react-native-gesture-handler';
 import axios from 'axios'
-    
-  const windowWidth = Dimensions.get('window').width
-  const windowHeight = Dimensions.get('window').height
 
-export default function report({ navigation, route }) {    
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
+
+export default function report({ navigation, route }) {
   const [report, setReport] = useState({
     name:'',
     latitude:0,
     longitude:0,
     type:""
   });
-  const [text, setText] = useState(null);
   const [name, setName] = useState(null);
   const [type, setType] = useState(null);
-  const {latitude, longitude} = route.params;  
+  const {latitude, longitude} = route.params;
+  const [isLoading, setIsLoading] = useState(false)
   console.log(latitude, longitude)
+  useEffect(()=>{
+    setReport({
+      name:name,
+      latitude:latitude,
+      longitude:longitude,
+      type:type
+    })
+  }, [name, type])
 
   const handleSubmit = async () => {
-    console.log('this is name:', name, 'this is type:',  type)
-    console.log('submit')
-    if (name != null && type != null) {
-      console.log('Not Empty');
-      setReport({
-        name:name,
-        latitude:latitude,
-        longitude:longitude,
-        type:type
-      });
+    console.log('This is name:', name, 'This is Type:', type);
+    setIsLoading(true);
+    console.log('submit');
+    if (name != null && type != null){
+      console.log('Text is not null');
       await axios.post('https://us-central1-project-base-74c62.cloudfunctions.net/api/report/add', report)
           .then(function (response) {
-            console.log(response)
-            navigation.navigate('Home')
+            setIsLoading(false)
           })
           .catch(function (error) {
-            console.log(error)
-          })
-      alert('สำเร็จ')
-    } else{
-      console.log('is empty')
+          });
+      if(isLoading == false){
+        navigation.navigate('Home')
+      }
+    }else {
+      console.log('Text is null');
       alert('โปรดกรอกข้อมูลให้ครบทุกช่อง')
     }
-};
-  
+  };
+
   return (
-    <View style={styles.container}>
-    <ScrollView>
-      <View style={styles.box}>        
-        <View style={styles.inBox}>
-        <Text style={styles.head}>Report</Text>
-        <View style={styles.coverText}>
-            <Text style={styles.textInside}>Name (เช่น ชื่อถนน, ชื่อสถานที่ เป็นต้น)</Text>
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.box}>
+            <View style={styles.inBox}>
+              <Text style={styles.head}>Report</Text>
+              <View style={styles.coverText}>
+                <Text style={styles.textInside}>Name (เช่น ชื่อถนน, ชื่อสถานที่ เป็นต้น)</Text>
+              </View>
+              <View>
+                <TextInput
+                    style={styles.textInput2}
+                    placeholder='ถนนพระราม2'
+                    value={name}
+                    onChangeText={name=>setName({name})}
+                />
+              </View>
+              <View style={styles.coverText}>
+                <Text style={styles.textInside}>Type (เช่น รถชน, ถนนลื่น เป็นต้น)</Text>
+              </View>
+              <View>
+                <TextInput
+                    style={styles.textInput2}
+                    placeholder='รถชน'
+                    value={type}
+                    onChangeText={type=>setType({type})}
+                />
+              </View>
+              <View style={styles.coverText}>
+                <Text style={styles.textInside}>Current location </Text>
+              </View>
+              <View>
+                <TextInput
+                    style={styles.textInput}
+                    value={`${latitude}, ${longitude}`}
+                    editable={false}
+                />
+              </View>
+              <View style={{width:'50%', marginLeft:'25%', marginTop:'8%'}}>
+                <Button
+                    color='#00CC00'
+                    title="Submit"
+                    onPress={handleSubmit}/>
+              </View>
+            </View>
           </View>
-          <View>
-              <TextInput
-              style={styles.textInput2}
-              value={name}
-              onChangeText={name=>setName({name})}
-              />
-          </View>
-          <View style={styles.coverText}>
-            <Text style={styles.textInside}>Type (เช่น รถชน, ถนนลื่น เป็นต้น)</Text>
-          </View>
-          <View>
-              <TextInput
-              style={styles.textInput2}
-              value={type}
-              onChangeText={type=>setType({type})}
-              />
-          </View>
-          <View style={styles.coverText}>
-            <Text style={styles.textInside}>Current location </Text>
-          </View>
-          <View>
-              <TextInput
-              style={styles.textInput}
-              value={`${latitude}, ${longitude}`}
-              editable={false}
-              />
-          </View>
-          <View style={{width:'50%', marginLeft:'25%', marginTop:'8%'}}>
-          <Button
-          color='#00CC00'
-          title="Submit"
-          onPress={handleSubmit}/>
-          </View>        
-        </View>
+        </ScrollView>
       </View>
-      </ScrollView>
-    </View>
-    
+
   );
 
 }
@@ -104,7 +109,7 @@ const styles = StyleSheet.create({
   },
   box:{
     backgroundColor:'#fff',
-    width:'99%',
+    width:'100%',
     height:'85%',
     marginTop:windowHeight*0.08
   },
